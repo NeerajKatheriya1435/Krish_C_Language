d207698b772c14fa9de9b8775bedeb44ad21346b
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-06-11 12:35:36 +0530

    Practice questions and loops

---
+++ day5.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+
+int main()
+{
+    // printf("Hello my name \\n  \'is neeraj \nmy age is : 18");
+    // printf("Hello my name\t\t\tage is : 18");
+
+    // int age=37;
+
+    // if(age>18){
+    //     printf("You can drive the car\n");
+    //     if(age==33){
+    //         printf("You are in special age\n");
+    //     }else{
+    //         printf("You are not good");
+    //     }
+    // }else{
+    //     printf("You can not drive the car\n");
+    // }
+
+    // int age;
+
+    // printf("Enter your age: ");
+    // scanf("%d",&age);
+
+    // if(age<=0 || age>=130){
+    //     printf("Invalid age\n");
+    // }
+    // else if(age>=0 && age<18){
+    //     printf("You can not drive the car\n");
+    // }
+    // else if(age>=18 && age<80){
+    //     printf("You can drive the car\n");
+    // }
+    // else{
+    //     printf("You are overaged");
+    // }
+
+    // int marks = 18;
+
+    // if (marks >= 90)
+    // {
+    //     printf("Grade A");
+    // }
+    // else if (marks >= 75)
+    // {
+    //     printf("Grade B");
+    // }
+    // else if (marks >= 50)
+    // {
+    //     printf("Grade C");
+    // }
+    // else
+    // {
+    //     printf("Fail");
+    // }
+
+    // int day;
+
+    // printf("Enter the day: ");
+    // scanf("%d",&day);
+
+    // switch(day){
+    //     case 1:
+    //         printf("Sunday");
+    //         break;
+    //     case 2:
+    //         printf("Monday");
+    //         break;
+    //     case 3:
+    //         printf("Tuesday");
+    //         break;
+    //     default:
+    //         printf("Please input 1 to 3");
+    // }
+
+        // Simple calculator +,-,*,/ using switch case
+        // greatest number among 3 using else if
+
+
+    // printf("1\n");
+    // printf("2\n");
+    // printf("3\n");
+    // printf("4\n");
+    // printf("5\n");
+
+    // for(int i=0;i<100000; i++){
+    //     printf("%d\n",i);
+    // }
+
+
+    // int table=65768;
+    // for(int i=571;i<11; i++){
+    //     printf("%d\n",i*table);
+    // }
+
+    // int i=16678;
+    // while (i<11)
+    // {
+    //     printf("%d\n",i);
+    //     i++;
+    // }
+
+    // int i=5;
+    // do{
+    //     printf("%d\n",i);
+    //     i++;
+    // }while(i<11);
+
+    // for(int i=1;i<11; i++){
+    //     printf("%d\n",i);
+    //     if(i==5){
+    //         break;
+    //     }
+    // }
+    
+    // for(int i=1;i<11; i++){
+    //     if(i==5 || i==9){
+    //         continue;
+    //     }
+    //     printf("%d\n",i);
+    // }
+    
+    return 0;
+}
