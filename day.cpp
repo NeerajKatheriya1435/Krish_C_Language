b01b08b428f502c6066445bdd3d7a804906f0f07
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-06-29 12:59:49 +0530

    data types and operators

---
+++ day.cpp
@@ -0,0 +1,57 @@
+// #include <stdio.h>
+// #include <stlib.h>
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // cout<<"Hello My name is Neeraj"<<endl;
+    // int a=8;
+    // int b=5;
+    // cout<<"The sum is: "<<(a+b)<<endl;
+
+    /*
+    Hello i am good
+    nice to meet you
+    how are you
+    */
+//    bool t1=false;
+
+//    cout<<t1;
+
+    int age = 21; // Integer type
+    // float marks = 88.75; // Float type
+    // double pi = 3.14159; // Double type
+    // char grade = 'A'; // Character type
+    // bool isPass = true; // Boolean type
+
+    // cout << "Age: " << age << endl;
+    // cout << "Marks: " << marks << endl;
+    // cout << "Value of Pi: " << pi << endl;
+    // cout << "Grade: " << grade << endl;
+    // cout << "Pass Status: " << isPass << endl;
+
+    // int a=7;
+    // int b=13;
+    // int result = (a > b) ? a : b;
+    // cout<<"The value of result is: "<<result<<endl;
+
+    // cout << sizeof(double);
+
+    // int a = (2, 3,89,1); // a = 3
+    // cout<<a<<endl;
+
+    // cout << "(a > 5 && b < 5): " << (a > 5 && b < 5) << endl;
+    // cout << "(a > 5 || b < 5): " << (a > 5 || b < 5) << endl;
+
+    int a;
+    int b;
+    cout<<"Enter the First Num: "<<endl;
+    cin>>a;
+    cout<<"Enter the Second Num: "<<endl;
+    cin>>b;
+
+    cout<<"The sum is: "<<(a+b)<<endl;
+    return 0;
+}
